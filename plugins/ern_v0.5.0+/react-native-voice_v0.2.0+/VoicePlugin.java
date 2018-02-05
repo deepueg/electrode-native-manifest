6d48b9fcd74c2275835432f0da2dcef6d4cfdc45
Deepu Eswarath Ganapathiyadan <DGanapathiyadan@walmartlabs.com>
2018-02-05 14:06:45 -0800

    add react-native-voice plugin (https://github.com/wenkesj/react-native-voice)

---
+++ plugins/ern_v0.5.0+/react-native-voice_v0.2.0+/VoicePlugin.java
@@ -0,0 +1,18 @@
+package com.walmartlabs.ern.container.plugins;
+
+import android.app.Application;
+import android.support.annotation.NonNull;
+
+import com.facebook.react.ReactInstanceManagerBuilder;
+import com.facebook.react.ReactPackage;
+import com.wenkesj.voice.VoicePackage;
+
+public class VoicePlugin {
+
+    public ReactPackage hook(@NonNull Application application,
+                     @NonNull ReactInstanceManagerBuilder reactInstanceManagerBuilder) {
+        VoicePackage voicePackage = new VoicePackage();
+        reactInstanceManagerBuilder.addPackage(voicePackage);
+        return voicePackage;
+    }
+}
