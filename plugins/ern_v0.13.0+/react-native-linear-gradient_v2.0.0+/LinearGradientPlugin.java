4d5704e9ca9146c9b907ce299eaf42ca9f0bb292
Deepu Eswarath Ganapathiyadan <DGanapathiyadan@walmartlabs.com>
2018-02-07 16:03:02 -0800

    add nullable import

--- plugins/ern_v0.13.0+/react-native-linear-gradient_v2.0.0+/LinearGradientPlugin.java
+++ plugins/ern_v0.13.0+/react-native-linear-gradient_v2.0.0+/LinearGradientPlugin.java
@@ -2,6 +2,7 @@ package com.walmartlabs.ern.container.plugins;
 
 import android.app.Application;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 
 import com.facebook.react.ReactInstanceManagerBuilder;
 import com.facebook.react.ReactPackage;
